349d8bdc8d0aa5c2805e8c70d7c5081b7827501c
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-04-17 20:23:06 +0300

    Add new features

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
+import sys
 from web3 import Web3
+from web3.eth import wait_for_transaction_receipt
 import argparse
-from utils import create_new_account
+import utils
 
 URL = "http://127.0.0.1:8545"
 
 w3 = Web3(Web3.HTTPProvider(URL))
 
 ACCOUNT_DB_NAME = 'account.json'
+
+GAS_PRICE = utils.get_actual_gas_price(w3)
+
+contracts = {'token':  ('contracts/ERC20Token.sol', 'ERC20Token'),
+             'wallet': ('contracts/ServiceProviderWallet.sol', 'ServiceProviderWallet'),
+             'mgmt':   ('contracts/ManagementContract.sol', 'ManagementContract'),
+             'battery':('contracts/BatteryManagement.sol', 'BatteryManagement')}
+
+
+def _deploy_contract_and_wait(_actor, _contract_src_file, _contract_name, args=None):
+    tx_hash = _deploy_contract(_actor, _contract_src_file, _contract_name, args)
+    receipt = wait_for_transaction_receipt(w3, tx_hash, 120, 0.1)
+
+    return receipt.contractAdress
+
+
+def _deploy_contract(_actor, _contract_src_file, _contract_name, args=None):
+    compiled = utils.compile_contracts(_contract_src_file)
+    contract = utils.initialize_contract_factory(w3, compiled, _contract_src_file + ":" + _contract_name)
+
+    tx = {'from': _actor, 'gasPrice': GAS_PRICE}
+
+    return contract.deploy(transaction=tx, args=args)
 
 
 def create_parser():
@@ -18,7 +43,31 @@ def create_parser():
         help='Add account for software development company'
     )
 
+    parser.add_argument('--setup', type=float, required=False,
+        help='Deploy contract(s) to the chain. Set fee (in ether)' 
+             'for registration of one battery, which reflects'
+             'vendor registration fee'
+    )
+
     return parser
+
+
+def setup(_service_fee):
+    service_fee = w3.toWei(_service_fee, 'ether')
+
+    data = utils.open_data_base(ACCOUNT_DB_NAME)
+
+    if data is None:
+        print("Can't access account database")
+        return
+
+    actor = data['account']
+
+    utils.unlock_account(w3, actor, data['password'])
+
+    txd = {}
+    for i in ['token', 'wallet']:
+        txd[i] = _deploy_contract(actor, contracts[i][0], contracts[i][1])
 
 
 def main():
@@ -26,7 +75,11 @@ def main():
     args = parser.parse_args()
 
     if args.new:
-        print(create_new_account(w3, args.new, ACCOUNT_DB_NAME))
+        print(utils.create_new_account(w3, args.new, ACCOUNT_DB_NAME))
+    elif args.setup:
+        setup(args.setup)
+    else:
+        sys.exit("No parameters provided")
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
-import json
 import os
+import json
+from solc import compile_files
+
+
+def get_actual_gas_price(_w3):
+    return _w3.toWei(1, 'gwei')
 
 
 def write_data_base(_data, _file):
     with open(_file, 'w') as out:
         json.dump(_data, out)
+
+
+def unlock_account(_w3, _account, _password):
+    _w3.personal.unlockAccount(_account, _password, 60)
 
 
 def create_new_account(_w3, _password, _file):
@@ -17,3 +26,36 @@ def create_new_account(_w3, _password, _file):
 
     return data['account']
 
+
+def open_data_base(_file):
+    if os.path.exists(_file):
+        with open(_file) as file:
+            return json.load(file)
+
+    else:
+        return None
+
+
+def compile_contracts(_files):
+    if isinstance(_files, str):
+        contracts = compile_files([_files])
+
+    if isinstance(_files, list):
+        contracts = compile_files(_files)
+
+    return contracts
+
+
+def initialize_contract_factory(_w3, _compiled_contracts, _key, _address=None):
+    if _address is None:
+        contract = _w3.eth.contract(
+            abi=_compiled_contracts[_key]['abi'],
+            bytecode=_compiled_contracts[_key]['bin']
+        )
+    else:
+        contract = _w3.eth.contract(
+            abi=_compiled_contracts[_key]['abi'],
+            address=_address
+        )
+
+    return contract
