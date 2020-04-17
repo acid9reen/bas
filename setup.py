61dbcc1155b0dafd47a04812a260803364bd6aa7
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-04-17 22:10:40 +0300

    Add contracts registration

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import sys
+import web3
 from web3 import Web3
-from web3.eth import wait_for_transaction_receipt
+from web3._utils.threads import Timeout
+from random import random
 import argparse
 import utils
 
@@ -9,23 +11,24 @@ URL = "http://127.0.0.1:8545"
 w3 = Web3(Web3.HTTPProvider(URL))
 
 ACCOUNT_DB_NAME = 'account.json'
+MGMT_CONTRACT_DB_NAME = 'database.json'
 
 GAS_PRICE = utils.get_actual_gas_price(w3)
 
-contracts = {'token':  ('contracts/ERC20Token.sol', 'ERC20Token'),
+CONTRACTS = {'token':  ('contracts/ERC20Token.sol', 'ERC20Token'),
              'wallet': ('contracts/ServiceProviderWallet.sol', 'ServiceProviderWallet'),
              'mgmt':   ('contracts/ManagementContract.sol', 'ManagementContract'),
              'battery':('contracts/BatteryManagement.sol', 'BatteryManagement')}
 
 
-def _deploy_contract_and_wait(_actor, _contract_src_file, _contract_name, args=None):
+def _deploy_contract_and_wait(_actor:str, _contract_src_file:str, _contract_name:str, args=None): # return type?
     tx_hash = _deploy_contract(_actor, _contract_src_file, _contract_name, args)
-    receipt = wait_for_transaction_receipt(w3, tx_hash, 120, 0.1)
+    receipt = web3.eth.wait_for_transaction_receipt(w3, tx_hash, 120, 0.1)
 
     return receipt.contractAdress
 
 
-def _deploy_contract(_actor, _contract_src_file, _contract_name, args=None):
+def _deploy_contract(_actor:str, _contract_src_file:str, _contract_name:str, args=None): # return type?
     compiled = utils.compile_contracts(_contract_src_file)
     contract = utils.initialize_contract_factory(w3, compiled, _contract_src_file + ":" + _contract_name)
 
@@ -34,7 +37,7 @@ def _deploy_contract(_actor, _contract_src_file, _contract_name, args=None):
     return contract.deploy(transaction=tx, args=args)
 
 
-def create_parser():
+def create_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description = 'Battery authentication system tool'
     )
@@ -52,7 +55,35 @@ def create_parser():
     return parser
 
 
-def setup(_service_fee):
+def _wait_for_validation(_w3:Web3, _tx_dict:dict, _tmout:int=120) -> dict:
+    receipts_list = {}
+    
+    for i in _tx_dict.keys():
+        receipts_list[i] = [_tx_dict[i], None]
+    
+    confirmations = len(list(_tx_dict))
+
+    with Timeout(_tmout) as tm:
+        while(confirmations > 0):
+            for i in _tx_dict.keys():
+                if receipts_list[i][1] is None:
+                    txn_reciept = _w3.eth.getTransactionReceipt(receipts_list[i][0])
+
+                    if txn_reciept is not None:
+                        receipts_list[i][1] = txn_reciept
+                        confirmations -= 1
+                
+                tm.sleep(random())
+    
+    return receipts_list
+
+
+def _create_mgmt_contract_db(_contract_address:str) -> None:
+    data = {'mgmt_contract': _contract_address}
+    utils.write_data_base(data, MGMT_CONTRACT_DB_NAME)
+
+
+def setup(_service_fee:float) -> None:
     service_fee = w3.toWei(_service_fee, 'ether')
 
     data = utils.open_data_base(ACCOUNT_DB_NAME)
@@ -65,12 +96,53 @@ def setup(_service_fee):
 
     utils.unlock_account(w3, actor, data['password'])
 
-    txd = {}
+    tx_dict = {}
+
     for i in ['token', 'wallet']:
-        txd[i] = _deploy_contract(actor, contracts[i][0], contracts[i][1])
+        tx_dict[i] = _deploy_contract(actor, CONTRACTS[i][0], CONTRACTS[i][1])
+
+    # wait for deployment transactions validation
+    receipt_dict = _wait_for_validation(web3, tx_dict)
+
+    currency_token_contract_addr = receipt_dict['token'][1].contractAdress
+    service_provider_wallet_addr = receipt_dict['wallet'][1].contractAdress
+
+    if (receipt_dict['token'][1] is not None) and (receipt_dict['wallet'][1] is not None):
+        currency_token_contract_addr = receipt_dict['token'][1].contractAdress
+        service_provider_wallet_addr = receipt_dict['wallet'][1].contractAdress
+
+        if service_provider_wallet_addr is not None:
+            # deploy managment contract
+            mgmt_contract_addr = _deploy_contract_and_wait(actor, CONTRACTS['mgmt'][0], CONTRACTS['mgmt'][1], 
+                                                           [service_provider_wallet_addr, service_fee])
+            
+            if mgmt_contract_addr is not None:
+                _create_mgmt_contract_db(mgmt_contract_addr)
+
+                # deploy battery managment
+                battery_mgmt_contract_addr = _deploy_contract_and_wait(actor, CONTRACTS['battery'][0], CONTRACTS['battery'][1],
+                                                                       [mgmt_contract_addr, currency_token_contract_addr])
+                
+                if battery_mgmt_contract_addr is not None:
+                    compiled_contract = utils.compile_contracts(CONTRACTS['mgmt'][0])
+                    mgmt_contract = utils.initialize_contract_factory(web3, compiled_contract,
+                                                                      CONTRACTS['mgmt'][0] + ':' + CONTRACTS['mgmt'][1],
+                                                                      mgmt_contract_addr)
+
+                    tx_hash = mgmt_contract.functions.setBatteryManagementContract(battery_mgmt_contract_addr).transact({'from': actor, 'gasPrice': GAS_PRICE})
+                    receipt = web3.eth.wait_for_transaction_receipt(web3, tx_hash, 120, 0.1)
+
+                    if receipt.status == 1:
+                        print('Management contract:', mgmt_contract_addr, sep=' ')
+                        print('Wallet contract:', service_provider_wallet_addr, sep=' ')
+                        print('Currency contract:', currency_token_contract_addr, sep=' ')
+
+                        return
+
+    print('Contracts deployment and configuration failed')
 
 
-def main():
+def main() -> None:
     parser = create_parser()
     args = parser.parse_args()
 
--- utils.py
+++ utils.py
@@ -1,42 +1,44 @@
 import os
 import json
+from typing import Union
+from web3 import Web3
 from solc import compile_files
 
 
-def get_actual_gas_price(_w3):
+def get_actual_gas_price(_w3:Web3) -> float:
     return _w3.toWei(1, 'gwei')
 
 
-def write_data_base(_data, _file):
-    with open(_file, 'w') as out:
+def write_data_base(_data:dict, _file_name:str) -> None:
+    with open(_file_name, 'w') as out:
         json.dump(_data, out)
 
 
-def unlock_account(_w3, _account, _password):
+def unlock_account(_w3:Web3, _account:str, _password:str) -> None:
     _w3.personal.unlockAccount(_account, _password, 60)
 
 
-def create_new_account(_w3, _password, _file):
-    if os.path.exists(_file):
-        os.remove(_file)
+def create_new_account(_w3:Web3, _password:str, _file_name:str) -> str:
+    if os.path.exists(_file_name):
+        os.remove(_file_name)
 
     account = _w3.geth.personal.newAccount(_password)
     data = {"account": account, "password": _password}
-    write_data_base(data, _file)
+    write_data_base(data, _file_name)
 
     return data['account']
 
 
-def open_data_base(_file):
-    if os.path.exists(_file):
-        with open(_file) as file:
+def open_data_base(_file_name:str) -> Union[json, None]:
+    if os.path.exists(_file_name):
+        with open(_file_name) as file:
             return json.load(file)
 
     else:
         return None
 
 
-def compile_contracts(_files):
+def compile_contracts(_files: Union[str, list]): # return type?
     if isinstance(_files, str):
         contracts = compile_files([_files])
 
@@ -46,7 +48,16 @@ def compile_contracts(_files):
     return contracts
 
 
-def initialize_contract_factory(_w3, _compiled_contracts, _key, _address=None):
+def get_account_from_db(_file_name:str) -> Union[str, None]:
+    data = open_data_base(_file_name)
+    
+    if data is None:
+        return None
+    
+    return data["account"]
+
+
+def initialize_contract_factory(_w3:Web3, _compiled_contracts, _key:str, _address:str=None): # return type?
     if _address is None:
         contract = _w3.eth.contract(
             abi=_compiled_contracts[_key]['abi'],
