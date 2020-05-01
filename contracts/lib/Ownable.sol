05427ef9c0da7726c59b1d475955fb752b5cf96d
tosha <tosharybking@gmail.com>
2020-05-01 13:36:50 +0300

    Fix require functions

--- contracts/lib/Ownable.sol
+++ contracts/lib/Ownable.sol
@@ -24,7 +24,7 @@ contract Ownable {
    * @dev Throws if called by any account other than the owner.
    */
   modifier onlyOwner() {
-    require(msg.sender == owner);
+    require(msg.sender == owner, "Just owner can call contract");
     _;
   }
 
@@ -33,7 +33,7 @@ contract Ownable {
    * @param newOwner The address to transfer ownership to.
    */
   function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
+    require(newOwner != address(0), "The same owner");
     emit OwnershipTransferred(owner, newOwner);
     owner = newOwner;
   }
