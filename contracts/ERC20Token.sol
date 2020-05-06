c7d1dfd912327b612b0666681990819ce1f8dce4
tosha <tosharybking@gmail.com>
2020-05-06 11:44:31 +0300

    Add contract from std libs

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.6.4;
 
+import "./erc20/MintableToken.sol";
 
-contract ERC20Token {
-
+contract ERC20Token is MintableToken {
     constructor() public {
+        mint(owner, 100 ether);
     }
 }
